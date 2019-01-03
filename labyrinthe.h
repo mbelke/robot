2592d697e6ce0a8a0790b4904b0c663d3238ead3
SAGNA01 <sagnaboubou01@gmail.com>
2019-01-04 00:02:23 +0100

    commit

---
+++ labyrinthe.h
@@ -0,0 +1,21 @@
+#ifndef LABYRINTHE_H
+#define LABYRINTHE_H
+
+
+class labyrinthe {
+   public:
+       labyrinthe(int taille);
+       void creerLabyrinthe(int taille);
+
+   private:
+    int d_taille;
+
+};
+
+
+
+
+
+
+
+#endif // LABYRINTHE
