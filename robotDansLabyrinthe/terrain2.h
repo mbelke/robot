93ef8ef656fd501537f12e07a9e6b795db94bec4
SAGNA01 <sagnaboubou01@gmail.com>
2019-01-06 01:46:39 +0100

    Add files via upload
    commit initial

--- robotDansLabyrinthe/terrain2.h
+++ robotDansLabyrinthe/terrain2.h
@@ -1,1 +1,25 @@
+#ifndef TERRAIN2_H
+#define TERRAIN2_H
 
+#include "math.h"
+
+class terrain2
+{
+
+
+
+    public:
+        terrain2();
+        entourerLabyrhinte();
+
+        void initialiser();
+        void dessinerLesLignes(char, long int, long int , long int , long int , long int);
+        void dessinerLabyrhinte(char, long, long, long, long, long, long, long);
+
+
+
+
+    private:
+};
+
+#endif // TERRAIN2_H
